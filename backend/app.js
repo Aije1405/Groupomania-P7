8e78aa7f36e65e17ce90fda180f7172192e8b9ff
Aije <andryjulie@hotmail.com>
2020-08-31 13:42:58 +0200

    installing body-parser, express and helmet

--- backend/app.js
+++ backend/app.js
@@ -2,3 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const helmet = require("helmet");
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use (helmet()); // L'application utilise toutes les protections helmet
+
+module.exports = app;
