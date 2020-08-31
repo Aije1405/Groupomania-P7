28bd918ea8ee713a2a7d90fa48de91df0bc905df
Aije <andryjulie@hotmail.com>
2020-08-31 15:50:58 +0200

    groupomania database and user table creation

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,20 @@ const helmet = require("helmet");
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+  
+    next();
+  });
+
 app.use(bodyParser.json());
 app.use (helmet()); // L'application utilise toutes les protections helmet
 
---
+++ backend/connectDbMySQL.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection ({
+    host: "localhost",
+    user: "root",
+    password: "Tamas2008",
+    database: "groupomania"
+  });
+  connection.connect ((err) => {
+    if (err) throw err;
+    console.log ('Connecté!');
+  });
+
+  module.exports = connection;
