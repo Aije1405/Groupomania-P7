e792955eaf4ce39e5555a7db8e35380fbf935adc
Aije <andryjulie@hotmail.com>
2020-09-07 21:52:38 +0200

    post nouveau frontend vue 2

---
+++ frontend/src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Accueil from '../views/accueil.vue'
+import Signup from '../views/signup.vue'
+import Login from '../views/login.vue'
+import User from '../views/user.vue'
+import Wall from '../views/wall.vue'
+
+Vue.use(VueRouter)
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'Accueil',
+    component: Accueil
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/user',
+    name: 'User',
+    component: User
+  },
+  {
+    path: '/wall',
+    name: 'Wall',
+    component: Wall
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+export default router 
